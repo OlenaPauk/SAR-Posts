2640ed258601a1b536e82430bcb2e39e032ba5d7
DESKTOP-9C48B5P\olenka <paukoleno4ka@gmail.com>
2021-03-19 21:10:47 +0200

    shared components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { PostsComponent } from './posts/posts.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'posts',component:PostsComponent},
+  {path:'gallery',component:GalleryComponent},
+  {path:'contact-us',component:ContactUsComponent},
+  {path:'**',redirectTo:'/'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
